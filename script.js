65d1dbb47f8ecf546b051d44bb10508b61f39f68
Sudhanshu Singh <sudsingh32@gmail.com>
2023-09-22 06:47:37 +0530

    Added ticket color functionality with lock and unlock content edittable vlaue.

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 let addTicket=document.querySelector(".fa-plus");
 let modalConatiner=document.querySelector(".modalContainer");
-let lock=document.querySelector(".fa-lock");
 let mainTicketContainer=document.querySelector(".ticketContainer");
 let textArea=document.querySelector("#textArea");
+
+// Get access of all priority colors
+let priorityColor=document.querySelectorAll(".priorityColor")
+
+let allColors=["pink","green","yellow","black"];
+let setColor =allColors[allColors.length - 1];
+
+// Event Listener for modal priority coloring
+
+priorityColor.forEach((colorElem, idx) => {
+    colorElem.addEventListener("click", (e) => {
+        priorityColor.forEach((priorityColorElem, idx) => {
+
+            priorityColorElem.classList.remove("defaultBorder");
+        })
+        colorElem.classList.add("defaultBorder");
+
+        setColor = colorElem.classList[1];
+
+    })
+})
+
+
 
 // Event Listener for modal display to add content
 addTicket.addEventListener("click",(e) => {
@@ -14,30 +36,49 @@ addTicket.addEventListener("click",(e) => {
 modalConatiner.addEventListener("keydown", (e) => {
     let key=e.key;
     if(key === "Enter"){
-        createTicket();
+        createTicket(setColor, textArea.value, shortid());
         modalConatiner.classList.toggle("modalDisplay");
         textArea.value="";
     }
 })
 
 // Creating a ticket and adding it in the main view.
-function createTicket(){
+function createTicket(ticketColor,taskValue,ticketID){
     let ticket=document.createElement("div");
     ticket.setAttribute("class","ticket");
     ticket.innerHTML=`
-                <div class="ticketColor"></div>
-                <div class="ticketId">#id</div>
-                <div class="taskArea">Lorem ipsum dolor sit amet consectetur aptio officiis eos voluptatum nobis tenetur doloribus hic atque expedita ullam ab eligendi aliquid dolore veritatis</div>
+                <div class="ticketColor ${ticketColor}"></div>
+                <div class="ticketId">#${ticketID}</div>
+                <div class="taskArea">${taskValue}</div>
                 <div class="lock">
                     <i class="fa-solid fa-lock"></i>
                 </div>
     `;
     mainTicketContainer.appendChild(ticket);
+
+    handleLock(ticket);
 }
 
+// When clicked on ticket color the color should be changed randomly : Use Math.random value
+
 // Locking the ticket area so that editing will be custom.
-lock.addEventListener("click",(e) => {
-    // toggle lock state: open/closed
-    lock.classList.toggle("fa-lock-open");
-});
+function handleLock(ticket){
+    let lockElem=ticket.querySelector(".lock");
+    let lockSwitch=lockElem.children[0];
+    let ticketTaskArea=ticket.querySelector(".taskArea");
+    lockSwitch.addEventListener("click" , (e) => {
+        if(lockSwitch.classList.contains("fa-lock")){
+            lockSwitch.classList.remove("fa-lock");
+            lockSwitch.classList.add("fa-lock-open");
+            ticketTaskArea.setAttribute("contenteditable", "true");
+        }
+        else{
+            lockSwitch.classList.add("fa-lock");
+            lockSwitch.classList.remove("fa-lock-open");
+            ticketTaskArea.setAttribute("contenteditable", "false");
+        }
+
+    })
+}
+
 
